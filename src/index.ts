76de2762edbb98d51bed90c9e8e29ad31b93b34c
JulioC090 <juliocziviani@gmail.com>
2024-12-28 19:31:41 -0300

    feat: add OpenAI integration for automated commit message generation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('Hello World!');
+import { execSync } from 'node:child_process';
+import OpenAI from 'openai';
+
+const diff = execSync('git diff --staged').toString();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_KEY,
+});
+
+const completion = openai.chat.completions.create({
+  model: 'gpt-4o-mini',
+  messages: [
+    {
+      role: 'user',
+      content: `
+      Create a commit message in the Conventional Commits format for this diff:
+
+      ${diff}
+
+      As a response, just give the message:
+      <type>: <description>`,
+    },
+  ],
+});
+
+completion.then((response) => {
+  console.log(response.choices[0].message.content);
+});
