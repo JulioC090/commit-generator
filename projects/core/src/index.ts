4adec4006d0b4e2e28c1f0ccdcdb0cd4ebcb1e1d
JulioC090 <juliocziviani@gmail.com>
2025-02-11 14:47:05 -0300

    refactor(core): rename factory files to use-cases for better organization

--- projects/core/src/index.ts
+++ projects/core/src/index.ts
@@ -1,5 +1,5 @@
-export { default as createAmendGenerated } from '@/application/factories/createAmendGenerated';
-export { default as createCommitGenerated } from '@/application/factories/createCommitGenerated';
-export { default as createEditLastGenerated } from '@/application/factories/createEditLastGenerated';
-export { default as createGenerateCommit } from '@/application/factories/createGenerateCommit';
-export { default as createValidateCommit } from '@/application/factories/createValidateCommit';
+export { default as createAmendGenerated } from '@/application/factories/use-cases/createAmendGenerated';
+export { default as createCommitGenerated } from '@/application/factories/use-cases/createCommitGenerated';
+export { default as createEditLastGenerated } from '@/application/factories/use-cases/createEditLastGenerated';
+export { default as createGenerateCommit } from '@/application/factories/use-cases/createGenerateCommit';
+export { default as createValidateCommit } from '@/application/factories/use-cases/createValidateCommit';
