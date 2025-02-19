64d685ad9a4dc7ca93d74abdfaeef58361e5f92d
JulioC090 <juliocziviani@gmail.com>
2025-02-19 20:45:36 -0300

    feat(ai-models): add new parameters to OllamaModel

--- packages/ai-models/src/models/OllamaModel.ts
+++ packages/ai-models/src/models/OllamaModel.ts
@@ -1,27 +1,39 @@
 import AIModel from '@/AIModel';
 import { IAIModelParams } from '@/types/IAIModelsParams';
-import ollama from 'ollama';
+import { Ollama } from 'ollama';
 
 export type IOllamaParams = {
   model: string;
+  temperature?: string;
+  url?: string;
 };
 
 export const OllamaSchema = {
   type: 'object',
   properties: {
     model: { type: 'string' },
+    temperature: { type: 'string' },
+    url: { type: 'string' },
   },
   required: ['model'],
   additionalProperties: false,
 };
 
 export default class OllamaModel extends AIModel<IOllamaParams> {
+  private _model: Ollama;
+
   constructor(parameters: IAIModelParams) {
     super(parameters, OllamaSchema);
+    this._model = new Ollama({
+      host: this.parameters.url ?? 'http://127.0.0.1:11434',
+    });
   }
 
   public async complete(prompt: string): Promise<string> {
-    const completion = await ollama.chat({
+    const completion = await this._model.chat({
+      options: {
+        temperature: Number(this.parameters.temperature),
+      },
       model: this.parameters.model,
       messages: [{ role: 'user', content: prompt }],
     });
